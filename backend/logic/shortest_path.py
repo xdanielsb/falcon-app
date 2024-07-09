b0c55c71b258c1d42304b11fa7af2d8fd72e1dfd
daniel <daniel.further@gmail.com>
2024-07-09 20:58:47 +0200

    feat: add initial structure project
    chore: add pre-commit with ruff for linting
    
    chore: add readme with instructions to setup a virtualenv.

---
+++ backend/logic/shortest_path.py
@@ -0,0 +1,15 @@
+"""
+Compute the shortest path from departure to destiny
+it receives a dictionary with the adjacency lists of the graph
+and the autonomy that reflects that the vehicle can travel
+without refueling autonomy units of distance without refueling
+"""
+
+
+def get_shortest_path_with_autonomy(
+    departure: int,
+    destiny: int,
+    adj_lists: dict[int, list[int]],
+    initial_autonomy: int | None = None,
+):
+    pass
---
+++ readme.md
@@ -0,0 +1,9 @@
+## Falcon backend
+
+### Create a virtualenv
+
+```sh
+  $ pyenv virtualenv 3.12 falcon-env 
+  $ pip3 install -r requirements.txt --no-cache-dir
+``
+
