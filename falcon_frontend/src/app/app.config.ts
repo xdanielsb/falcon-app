a4daf2f67c0735bc4ac3a2108815c66dba43f2f8
daniel <daniel.further@gmail.com>
2024-07-10 10:39:35 +0200

    feat: add internationalization.

--- falcon_frontend/src/app/app.config.ts
+++ falcon_frontend/src/app/app.config.ts
@@ -1,12 +1,35 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateModuleConfig } from '@ngx-translate/core/dist/public-api';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+const translateModuleConfig: TranslateModuleConfig = {
+  defaultLanguage: 'en',
+  loader: {
+    provide: TranslateLoader,
+    useFactory: createTranslateLoader,
+    deps: [HttpClient],
+  },
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
     provideRouter(appRoutes),
     provideAnimationsAsync(),
+    importProvidersFrom(TranslateModule.forRoot(translateModuleConfig)),
   ],
 };
--- falcon_frontend/src/app/components/graph-settings/graph-settings.component.ts
+++ falcon_frontend/src/app/components/graph-settings/graph-settings.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-graph-settings',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './graph-settings.component.html',
   styleUrl: './graph-settings.component.css',
 })
